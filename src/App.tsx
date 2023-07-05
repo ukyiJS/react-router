5beafe69ccef72968381560858ea88fe2f483dbd
ukyi <ukyi.js@gmail.com>
2023-07-06 01:53:25 +0900

    ✨ Router, Layout, Route 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
-export const App = (): JSX.Element => (
-  <main className="flex justify-center items-center h-screen">App</main>
-);
+import type { ReactNode } from 'react';
+import { Layout } from '@/components/layout';
+import { Index } from '@/pages';
+import { About } from '@/pages/About';
+import { Route, Router } from '@/router';
+
+export function App(): ReactNode {
+  return (
+      <Router>
+        <Layout>
+          <Route path="/" component={<Index />}></Route>
+          <Route path="/about" component={<About />}></Route>
+        </Layout>
+      </Router>
+  );
+}
