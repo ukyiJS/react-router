6ca4c84fe397414d596fbabf7ac5e006fc008c0e
ukyi <ukyi.js@gmail.com>
2023-07-06 01:53:08 +0900

    ✨ About 페이지 컴포넌트 추가

---
+++ src/pages/About.tsx
@@ -0,0 +1,9 @@
+import type { ReactNode } from 'react';
+
+export function About(): ReactNode {
+  return (
+    <section className="flex justify-center items-center h-screen">
+      <h1>About Page</h1>
+    </section>
+  );
+}
