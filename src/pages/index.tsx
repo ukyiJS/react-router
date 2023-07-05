d96b1334d4ea9740073eb86c1ccc7b60fde4f648
ukyi <ukyi.js@gmail.com>
2023-07-06 01:53:03 +0900

    ✨ Index 페이지 컴포넌트 추가

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import type { ReactNode } from 'react';
+
+export function Index(): ReactNode {
+  return (
+    <section className="flex justify-center items-center h-screen">
+      <h1>Index Page</h1>
+      <button></button>
+    </section>
+  );
+}
