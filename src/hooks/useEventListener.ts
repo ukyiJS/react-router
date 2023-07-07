fd3c33e3bd8819ec53cc3aeb9ddceae78656c1bf
ukyi <ukyi.js@gmail.com>
2023-07-07 16:19:15 +0900

    ♻️ lint fix

--- src/hooks/useEventListener.ts
+++ src/hooks/useEventListener.ts
@@ -1,11 +1,10 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect } from 'react';
 
-export type WindowEventName = keyof WindowEventMap
-
+export type WindowEventName = keyof WindowEventMap;
 export type EventListener<E extends WindowEventName> = {
   (this: Window, ev: WindowEventMap[E]): void;
-}
+};
 
 export const useEventListener = <E extends WindowEventName>(event: E, listener: EventListener<E>): void => {
   useEffect(() => {
